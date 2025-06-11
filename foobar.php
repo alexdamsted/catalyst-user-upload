f6ce36d48929e69d3b5de5257b87dea7ad3f6afc
root <root@Alexd.localdomain>
2025-06-11 22:53:34 +1200

    more divisble logic for 5 and 3 && 5

--- foobar.php
+++ foobar.php
@@ -2,16 +2,22 @@
 
 
 for($i = 1; $i <= 100; $i++){
-	if ($i%3 == 0) { 
+	if (($i%3 == 0) && ($i%5 == 0)) {
+		echo "foobar" . ", ";
+		continue;
+	} else if ($i%3 == 0) { 
 		echo "foo" . ", "; 
+		continue;
+	} else if ($i%5 == 0) {
+		echo "bar" . ", ";
 		continue;
 	}
 
-		if ($i !== 100) {
-			echo $i . ", ";
-		} else {	
-			echo $i . "\n";
-		}
+	if ($i !== 100) {
+		echo $i . ", ";
+	} else {
+		echo $i . "\n";
+	}
 }
 
 

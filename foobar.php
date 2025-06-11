20bec7aedaf463b96e32604fe8bf601898687ba0
root <root@Alexd.localdomain>
2025-06-11 22:46:30 +1200

    use modulus to check if divisible by 3

--- foobar.php
+++ foobar.php
@@ -2,11 +2,16 @@
 
 
 for($i = 1; $i <= 100; $i++){
-	if ($i !== 100) {
-		echo $i . ", ";
-	} else {	
-		echo $i . "\n";
+	if ($i%3 == 0) { 
+		echo "foo" . ", "; 
+		continue;
 	}
+
+		if ($i !== 100) {
+			echo $i . ", ";
+		} else {	
+			echo $i . "\n";
+		}
 }
 
 

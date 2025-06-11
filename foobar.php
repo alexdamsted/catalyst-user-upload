7ed3b89283114b0b5ef916ddd3bdb1a1e441a115
root <root@Alexd.localdomain>
2025-06-11 23:02:53 +1200

    bug: remove the trailing comma from the output when  is 100

--- foobar.php
+++ foobar.php
@@ -3,14 +3,29 @@
 
 for($i = 1; $i <= 100; $i++){
 	if (($i%3 == 0) && ($i%5 == 0)) {
-		echo "foobar" . ", ";
-		continue;
+		if ($i !== 100) {
+			echo "foobar" . ", ";
+			continue;
+		} else {
+			echo "foobar" . "\n";
+			continue;
+		}
 	} else if ($i%3 == 0) { 
-		echo "foo" . ", "; 
-		continue;
+		if ($i !== 100) {
+			echo "foo" . ", "; 
+			continue;
+		} else {
+			echo "foo" . "\n"; 
+			continue;
+		}
 	} else if ($i%5 == 0) {
-		echo "bar" . ", ";
-		continue;
+		if ($i !== 100) {
+			echo "bar" . ", ";
+			continue;
+		} else {
+			echo "bar" . "\n";
+			continue;
+		}
 	}
 
 	if ($i !== 100) {
